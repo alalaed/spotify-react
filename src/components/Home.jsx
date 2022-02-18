23ad687ff185fa8f916be49ebd6bfb9ef3bfb8b9
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-02-18 14:12:32 +0100

    home and player done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,35 @@
+import { Container, Row } from "react-bootstrap";
+
 const Home = () => {
-  return <div>hello Home</div>;
+  return (
+    <div>
+      <Container fluid className="px-5" style={{ marginTop: "6em" }}>
+        <Row className=" ml-2">
+          <h1 className="h1-main">Good morning</h1>
+        </Row>
+        <Row id="main-section-small"></Row>
+      </Container>
+
+      <Container fluid className=" px-5 my-4">
+        <Row className="row ml-2">
+          <h2 className="h2-main">Recently played</h2>
+        </Row>
+        <Row id="main-section-recent"></Row>
+      </Container>
+
+      <Container fluid className=" px-5 my-4">
+        <Row className=" ml-2">
+          <h2 className="h2-main my-0">Shows to try</h2>
+        </Row>
+        <p className="ml-2 to-try-main text-left">
+          Podcasts we think you will get hooked on.
+        </p>
+        <div id="main-section-to-try" className="row"></div>
+      </Container>
+
+      <div style={{ height: "100px" }}></div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/MyPlayer.jsx
+++ src/components/MyPlayer.jsx
@@ -1,5 +1,97 @@
+import { Container, Row, col, Col } from "react-bootstrap";
+import tile1 from "../assets/tile1.jpg";
+import {
+  BsHeart,
+  BsPip,
+  BsShuffle,
+  BsFillSkipStartFill,
+  BsFillPlayCircleFill,
+  BsFillSkipEndFill,
+  BsArrowRepeat,
+  BsListUl,
+  BsMusicPlayer,
+  BsFillVolumeUpFill,
+} from "react-icons/bs";
+
 const MyPlayer = () => {
-  return <div>hello Player</div>;
+  return (
+    <div>
+      <footer className="fixed-bottom footer-class">
+        <Container fluid>
+          <Row>
+            <Col className="col-md-3 d-flex flex-nowrap justify-content-center justify-content-md-between">
+              <img
+                src={tile1}
+                alt=""
+                className="align-self-center footer-tile mr-2"
+              />
+
+              <div className="align-self-center mr-md-auto">
+                <p
+                  className="mb-0 footer-song-text"
+                  style={{ fontSize: "small" }}
+                >
+                  Everlong
+                </p>
+                <span className="lead" style={{ fontSize: "x-small" }}>
+                  Foo Fighters
+                </span>
+              </div>
+              <div className="d-flex align-items-center justify-content-center">
+                <BsHeart className="ml-3" />
+                <BsPip className="ml-3" />
+              </div>
+            </Col>
+            <Col md={6} className="px-5">
+              <div className="d-flex justify-content-center align-items-center w-100">
+                <BsShuffle className="d-inline-block mx-2 " />
+                <BsFillSkipStartFill className="d-inline-block mx-2 " />
+                <div className="footer-play-button-wrapper">
+                  <BsFillPlayCircleFill className="d-inline-block footer-play-button-bigger mx-2" />
+                </div>
+                <BsFillSkipEndFill className="d-inline-block mx-2" />
+                <BsArrowRepeat d-inline-block mx-2 />
+              </div>
+              <div className="d-flex align-items-center justify-content-center mb-3 mt-n1">
+                <span className="mr-n4" style={{ fontSize: "x-small" }}>
+                  3.34
+                </span>
+                <div style={{ width: "80%" }}>
+                  <hr
+                    className=" align-self-start mb-auto mt-auto"
+                    style={{
+                      height: " 3px",
+                      width: "80%",
+                      backgroundColor: "grey",
+                      borderRadius: "2px",
+                    }}
+                  />
+                </div>
+                <span className="ml-n4" style={{ fontSize: "x-small" }}>
+                  3.34
+                </span>
+              </div>
+            </Col>
+            <Col md={2} className="d-none d-lg-block ">
+              <div className="d-flex flex-nowrap justify-content-between align-items-center d-inline-block mt-3">
+                <BsListUl />
+                <BsMusicPlayer className="px-2" />
+                <BsFillVolumeUpFill className="pl-2" />
+                <hr
+                  style={{
+                    height: "3px",
+                    width: "60%",
+                    backgroundColor: "grey",
+                    borderRadius: "2px",
+                  }}
+                />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </div>
+  );
 };
 
 export default MyPlayer;
